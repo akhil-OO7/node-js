1f031c362e67f233eb27c7b7b18af74243854ba9
Akhil Chauhan <akthehacker11@gmail.com>
2023-07-22 19:31:03 +0530

    lesson-9

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://testuser1:test1234@cluster0.qnf8alw.mongodb.net/cluster0?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then((res) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
+// mongoose and mondo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("64bbdb9233e30bc2541ea5b3")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //custom middle ware
 // app.use((req, res, next) => {
@@ -23,21 +68,7 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 // app.use((req, res, next) => {
@@ -47,6 +78,18 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
