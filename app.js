53533eee382bff635c3a33358906b45d5549f23f
Akhil Chauhan <akthehacker11@gmail.com>
2023-07-22 13:00:46 +0530

    lesson-7

--- app.js
+++ app.js
@@ -3,17 +3,35 @@ const express = require("express");
 // express app
 const app = express();
 
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // res.send("<p>Home Page</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
-  // res.send("<p>About Page</p>");
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 app.get("/about-us", (req, res) => {
@@ -21,5 +39,5 @@ app.get("/about-us", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
